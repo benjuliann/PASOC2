a208a5e39a7df6dc723e3b11cbb18a4b2592e9f8
Joseph <jcortes11100@gmail.com>
2026-02-12 20:07:07 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,11 +2,11 @@
 import * as React from "react";
 import { HeroSection } from "./UI/HeroSection.jsx";
 import SocialFunctions from "./UI/SocialFunctions.jsx";
+import { FloatingButton } from "./UI/FloatingButton";
 // import { SponsorSection } from "./Frontend/Home/SponsorSection";
 // import { NewsSection } from "./Frontend/Home/NewsSection";
 // import { ScholarsSection } from "./Frontend/Home/ScholarsSection";
 // import { GallerySection } from "./Frontend/Home/GallerySection";
-// import { FloatingButton } from "./Frontend/FloatingButton";
 
 function App() {
 	return (
