28e4d641449d4518256efbaa05d17a73a6322dba
yvnmnl <yvana.gaddao@edu.sait.ca>
2026-02-18 00:10:55 -0700

    Make Login page responsive and scrollable
    made screen responsive

--- app/Pages/Login/page.js
+++ app/Pages/Login/page.js
@@ -7,7 +7,7 @@ export default function LoginPage() {
   };
 
   return (
-    <main className="h-screen bg-[#F4EFE7] relative overflow-hidden">
+    <main className="min-h-dvh bg-[#F4EFE7] relative overflow-y-auto md:overflow-hidden">
       
       {/* Back Arrow */}
       <Link href="/" className="absolute left-6 top-6">
@@ -25,7 +25,7 @@ export default function LoginPage() {
         </svg>
       </Link>
 
-      <div className="h-screen px-4 flex justify-center items-center">
+      <div className="min-h-dvh px-4 flex justify-center items-center">
         <div className="w-full max-w-[560px] flex flex-col items-center scale-90">
           
           {/* PASOC Logo */}
@@ -33,7 +33,7 @@ export default function LoginPage() {
             <img
               src="https://api.builder.io/api/v1/image/assets/TEMP/59857bbe636f97ab4cebcfbc3ad030ee40910eb4?placeholderIfAbsent=true&apiKey=fbbee8c7a138402fba2a2964fb2f753d"
               alt="PASOC Logo"
-              className="w-[360px] max-w-[85vw] mb-6"
+              className="w-[260px] sm:w-[320px] md:w-[360px] max-w-[85vw] mb-6"
             />
           </Link>
 
