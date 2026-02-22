1e5eb24a14d7cf9cb1e398a90256e83a8d632203
thatasiandude <benjulian.noel@gmail.com>
2026-02-21 23:52:05 -0700

    next/head experiment

--- app/page.js
+++ app/page.js
@@ -2,10 +2,15 @@
 import * as React from "react";
 import { HeroSection } from "./UI/HeroSection.jsx";
 import { FloatingButton } from "./UI/FloatingButton.jsx";
+import Head from "next/head";
 
 function App() {
 	return (
 		<div style={{ display: "flex", flexDirection: "column"}}>
+			<Head>
+        		<title>My page title</title>
+				
+      		</Head>
 			<FloatingButton />
 			<div className="flex flex-col">
 				<HeroSection />
