73661d158a1a56eb38585f9353a6751166e30c83
Joseph <jcortes11100@gmail.com>
2026-02-24 17:04:00 -0700

    Updated the Hero section

---
+++ app/Pages/faqs/page.js
@@ -0,0 +1,13 @@
+import { HeroSection } from "../../UI/HeroSection.jsx";
+
+export default function FAQsPage() {
+	return (
+		<main>
+			<HeroSection
+				heading="Frequently Asked Questions"
+				greeting=""
+				showButtons={false}
+			/>
+		</main>
+	);
+}
